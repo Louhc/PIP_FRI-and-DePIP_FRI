9d56218c5a0e482be1780f3b428796a9b437ec8f
Pengfei Zhu <zhupf321@gmail.com>
2024-11-10 13:13:50 +0800

    Reapply "test bls12 381"
    This reverts commit 3c30dbbfac9f540447315b62a1fcd02cb35ce21b.

--- simple-payments/src/lib.rs
+++ simple-payments/src/lib.rs
@@ -1,12 +1,12 @@
-// pub use ark_ed_on_bls12_381::EdwardsProjective;
-// pub use ark_ed_on_bls12_381::constraints::EdwardsVar;
-// pub type ConstraintFq = ark_ed_on_bls12_381::Fq;
-// pub type ConstraintF = ark_bls12_381::Fr;
+pub use ark_ed_on_bls12_381::EdwardsProjective;
+pub use ark_ed_on_bls12_381::constraints::EdwardsVar;
+pub type ConstraintFq = ark_ed_on_bls12_381::Fq;
+pub type ConstraintF = ark_bls12_381::Fr;
 
-pub use ark_ed_on_bn254::EdwardsProjective;
-pub use ark_ed_on_bn254::constraints::EdwardsVar;
-pub type ConstraintFq = ark_ed_on_bn254::Fq;
-pub type ConstraintF = ark_bn254::Fr;
+// pub use ark_ed_on_bn254::EdwardsProjective;
+// pub use ark_ed_on_bn254::constraints::EdwardsVar;
+// pub type ConstraintFq = ark_ed_on_bn254::Fq;
+// pub type ConstraintF = ark_bls12_381::Fr;
 
 pub mod account;
 pub mod ledger;
