87e039779097370ce4075fe81db227576c79b444
gby2023 <boyuan.gao@outlook.com>
2025-03-28 17:52:59 +0800

    update

--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -1,13 +1,13 @@
 pub mod fp17;
 // pub mod fp64;
 
-pub mod merkle_tree;
-pub mod helper;
-pub mod goldilocks;
-pub mod query_result;
-pub mod fiat_shamir;
 pub mod commit_open_vec;
+pub mod fiat_shamir;
+pub mod goldilocks;
+pub mod helper;
 pub mod interpolate_vecs_value;
+pub mod merkle_tree;
+pub mod query_result;
 
 pub const CODE_RATE: usize = 3;
 pub const SECURITY_BITS: usize = 100;
--- virgo/benches/bench.rs
+++ virgo/benches/bench.rs
@@ -1,26 +1,25 @@
 extern crate criterion;
-use std::mem;
-use utils::merkle_tree::MERKLE_ROOT_SIZE;
-use criterion::*;
 use ark_ff::UniformRand;
 use ark_poly::EvaluationDomain;
-use utils::helper::MultilinearPolynomial;
-use utils::fiat_shamir::RandomOracle;
-use utils::{CODE_RATE, SECURITY_BITS};
-use utils::goldilocks::Goldilocks as T;
-use utils::helper::Helper;
+use criterion::*;
 use rand::rngs::StdRng;
 use rand::SeedableRng;
-use virgo::{prover::FriProver,
-    verifier::FriVerifier,
-    zkprover::ZKFriProver,
-    zkverifier::ZKFriVerifier};
+use std::mem;
+use utils::fiat_shamir::RandomOracle;
+use utils::goldilocks::Goldilocks as T;
+use utils::helper::Helper;
+use utils::helper::MultilinearPolynomial;
+use utils::merkle_tree::MERKLE_ROOT_SIZE;
+use utils::{CODE_RATE, SECURITY_BITS};
+use virgo::{
+    prover::FriProver, verifier::FriVerifier, zkprover::ZKFriProver, zkverifier::ZKFriVerifier,
+};
 // usage:
 // RAYON_NUM_THREADS=8 cargo bench -p virgo
 // RAYON_NUM_THREADS=8 cargo bench --features "parallel" -p virgo
 
-const SMALL: usize = 20;
-const SIZE: usize = 25;
+const SMALL: usize = 22;
+const SIZE: usize = 22;
 
 fn commit(criterion: &mut Criterion, variable_num: usize) {
     let mut rng = StdRng::seed_from_u64(0u64);
@@ -29,15 +28,14 @@ fn commit(criterion: &mut Criterion, variable_num: usize) {
         .map(|_| T::rand(&mut rng))
         .collect::<Vec<T>>();
     let _evaluation = polynomial.evaluate(&point);
-    let mut interpolate_cosets = vec![EvaluationDomain::new_coset(
-        1 << (variable_num + CODE_RATE),
-        T::from(1),
-    ).unwrap()];
+    let mut interpolate_cosets =
+        vec![EvaluationDomain::new_coset(1 << (variable_num + CODE_RATE), T::from(1)).unwrap()];
     for i in 1..variable_num {
-        interpolate_cosets.push(Helper::pow(&interpolate_cosets[i-1], 2));
+        interpolate_cosets.push(Helper::pow(&interpolate_cosets[i - 1], 2));
     }
     let random_oracle = RandomOracle::new(variable_num, SECURITY_BITS / CODE_RATE);
-    let vector_interpolation_coset = EvaluationDomain::new_coset(1 << variable_num, T::from(1)).unwrap();
+    let vector_interpolation_coset =
+        EvaluationDomain::new_coset(1 << variable_num, T::from(1)).unwrap();
 
     criterion.bench_function(&format!("virgo commit {}", variable_num), move |b| {
         b.iter_batched(
@@ -65,15 +63,14 @@ fn zk_commit(criterion: &mut Criterion, variable_num: usize) {
         .collect::<Vec<T>>();
     let _evaluation = polynomial.evaluate(&point);
     // must +2 for the security of FRI on s, s's degree is 2|H| + \lambda - 1
-    let mut interpolate_cosets = vec![EvaluationDomain::new_coset(
-        1 << (variable_num + CODE_RATE + 2),
-        T::from(1),
-    ).unwrap()];
+    let mut interpolate_cosets =
+        vec![EvaluationDomain::new_coset(1 << (variable_num + CODE_RATE + 2), T::from(1)).unwrap()];
     for i in 1..(variable_num + 2) {
-        interpolate_cosets.push(Helper::pow(&interpolate_cosets[i-1], 2));
+        interpolate_cosets.push(Helper::pow(&interpolate_cosets[i - 1], 2));
     }
     let random_oracle = RandomOracle::new(variable_num + 2, SECURITY_BITS / CODE_RATE);
-    let vector_interpolation_coset = EvaluationDomain::new_coset(1 << variable_num, T::from(1)).unwrap();
+    let vector_interpolation_coset =
+        EvaluationDomain::new_coset(1 << variable_num, T::from(1)).unwrap();
 
     criterion.bench_function(&format!("zk-virgo commit {}", variable_num), move |b| {
         b.iter_batched(
@@ -107,15 +104,14 @@ fn open(criterion: &mut Criterion, variable_num: usize) {
         .map(|_| T::rand(&mut rng))
         .collect::<Vec<T>>();
     let _evaluation = polynomial.evaluate(&point);
-    let mut interpolate_cosets = vec![EvaluationDomain::new_coset(
-        1 << (variable_num + CODE_RATE),
-        T::from(1),
-    ).unwrap()];
+    let mut interpolate_cosets =
+        vec![EvaluationDomain::new_coset(1 << (variable_num + CODE_RATE), T::from(1)).unwrap()];
     for i in 1..variable_num {
-        interpolate_cosets.push(Helper::pow(&interpolate_cosets[i-1], 2));
+        interpolate_cosets.push(Helper::pow(&interpolate_cosets[i - 1], 2));
     }
     let random_oracle = RandomOracle::new(variable_num, SECURITY_BITS / CODE_RATE);
-    let vector_interpolation_coset = EvaluationDomain::new_coset(1 << variable_num, T::from(1)).unwrap();
+    let vector_interpolation_coset =
+        EvaluationDomain::new_coset(1 << variable_num, T::from(1)).unwrap();
     let prover = FriProver::new(
         variable_num,
         &interpolate_cosets,
@@ -159,15 +155,14 @@ fn zk_open(criterion: &mut Criterion, variable_num: usize) {
         .collect::<Vec<T>>();
     let _evaluation = polynomial.evaluate(&point);
     // must +2 for the security of FRI on s, s's degree is 2|H| + \lambda - 1
-    let mut interpolate_cosets = vec![EvaluationDomain::new_coset(
-        1 << (variable_num + CODE_RATE + 2),
-        T::from(1),
-    ).unwrap()];
+    let mut interpolate_cosets =
+        vec![EvaluationDomain::new_coset(1 << (variable_num + CODE_RATE + 2), T::from(1)).unwrap()];
     for i in 1..(variable_num + 2) {
-        interpolate_cosets.push(Helper::pow(&interpolate_cosets[i-1], 2));
+        interpolate_cosets.push(Helper::pow(&interpolate_cosets[i - 1], 2));
     }
     let random_oracle = RandomOracle::new(variable_num + 2, SECURITY_BITS / CODE_RATE);
-    let vector_interpolation_coset = EvaluationDomain::new_coset(1 << variable_num, T::from(1)).unwrap();
+    let vector_interpolation_coset =
+        EvaluationDomain::new_coset(1 << variable_num, T::from(1)).unwrap();
     let prover = ZKFriProver::new(
         variable_num + 2,
         &interpolate_cosets,
@@ -217,15 +212,14 @@ fn verify(criterion: &mut Criterion, variable_num: usize) {
         .map(|_| T::rand(&mut rng))
         .collect::<Vec<T>>();
     let evaluation = polynomial.evaluate(&point);
-    let mut interpolate_cosets = vec![EvaluationDomain::new_coset(
-        1 << (variable_num + CODE_RATE),
-        T::from(1),
-    ).unwrap()];
+    let mut interpolate_cosets =
+        vec![EvaluationDomain::new_coset(1 << (variable_num + CODE_RATE), T::from(1)).unwrap()];
     for i in 1..variable_num {
-        interpolate_cosets.push(Helper::pow(&interpolate_cosets[i-1], 2));
+        interpolate_cosets.push(Helper::pow(&interpolate_cosets[i - 1], 2));
     }
     let random_oracle = RandomOracle::new(variable_num, SECURITY_BITS / CODE_RATE);
-    let vector_interpolation_coset = EvaluationDomain::new_coset(1 << variable_num, T::from(1)).unwrap();
+    let vector_interpolation_coset =
+        EvaluationDomain::new_coset(1 << variable_num, T::from(1)).unwrap();
     let mut prover = FriProver::new(
         variable_num,
         &interpolate_cosets,
@@ -253,11 +247,14 @@ fn verify(criterion: &mut Criterion, variable_num: usize) {
 
     // Proof size
     let proof_size = folding_proofs.iter().map(|x| x.proof_size()).sum::<usize>()
-        + function_proofs.iter().map(|x| x.proof_size()).sum::<usize>()
+        + function_proofs
+            .iter()
+            .map(|x| x.proof_size())
+            .sum::<usize>()
         + v_value.len() * (mem::size_of::<usize>() + size_of::<T>())
         + MERKLE_ROOT_SIZE * (variable_num + 1);
     println!("proof size is: {:?} KBytes", proof_size / 1024);
-    
+
     criterion.bench_function(&format!("virgo verify {}", variable_num), move |b| {
         b.iter(|| {
             let is_valid = verifier.verify(evaluation, &folding_proofs, &v_value, &function_proofs);
@@ -265,7 +262,6 @@ fn verify(criterion: &mut Criterion, variable_num: usize) {
         })
     });
 }
-
 
 fn zk_verify(criterion: &mut Criterion, variable_num: usize) {
     let mut rng = StdRng::seed_from_u64(0u64);
@@ -275,15 +271,14 @@ fn zk_verify(criterion: &mut Criterion, variable_num: usize) {
         .collect::<Vec<T>>();
     let evaluation = polynomial.evaluate(&point);
     // must +2 for the security of FRI on s, s's degree is 2|H| + \lambda - 1
-    let mut interpolate_cosets = vec![EvaluationDomain::new_coset(
-        1 << (variable_num + CODE_RATE + 2),
-        T::from(1),
-    ).unwrap()];
+    let mut interpolate_cosets =
+        vec![EvaluationDomain::new_coset(1 << (variable_num + CODE_RATE + 2), T::from(1)).unwrap()];
     for i in 1..(variable_num + 2) {
-        interpolate_cosets.push(Helper::pow(&interpolate_cosets[i-1], 2));
+        interpolate_cosets.push(Helper::pow(&interpolate_cosets[i - 1], 2));
     }
     let random_oracle = RandomOracle::new(variable_num + 2, SECURITY_BITS / CODE_RATE);
-    let vector_interpolation_coset = EvaluationDomain::new_coset(1 << variable_num, T::from(1)).unwrap();
+    let vector_interpolation_coset =
+        EvaluationDomain::new_coset(1 << variable_num, T::from(1)).unwrap();
     let mut prover = ZKFriProver::new(
         variable_num + 2,
         &interpolate_cosets,
@@ -314,12 +309,22 @@ fn zk_verify(criterion: &mut Criterion, variable_num: usize) {
         + function_h_proof.proof_size()
         + v_value.len() * (mem::size_of::<usize>() + size_of::<T>())
         + MERKLE_ROOT_SIZE * (variable_num + 1);
-    println!("proof size for {} variable is: {:?} KB", variable_num, proof_size / 1024);
+    println!(
+        "proof size for {} variable is: {:?} KB",
+        variable_num,
+        proof_size / 1024
+    );
 
     criterion.bench_function(&format!("zk-virgo verify {}", variable_num), move |b| {
         b.iter(|| {
             // Verify
-            let is_valid = verifier.verify(evaluation, &folding_proofs, &v_value, &function_us_proof, &function_h_proof);
+            let is_valid = verifier.verify(
+                evaluation,
+                &folding_proofs,
+                &v_value,
+                &function_us_proof,
+                &function_h_proof,
+            );
             assert!(is_valid);
         })
     });
@@ -335,9 +340,9 @@ fn bench_verify(c: &mut Criterion) {
 criterion_group! {
     name = benches;
     config = Criterion::default().sample_size(10);
-    targets = 
-    bench_commit, 
-    bench_open, 
+    targets =
+    bench_commit,
+    bench_open,
     bench_verify
 }
 
